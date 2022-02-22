625e7ec7814110fad0a8dd94a7a1289d546eb3c7
Fay Johnston <swewrozka@gmail.com>
2022-02-22 15:35:04 -0500

    initialize laravel gates for access and Json resources for response data
    validate input data w/ formrequest
    
    parse yaml to php for api spec

---
+++ project-anemone/app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,23 @@
+namespace App\Http\Requests;
+
+use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+class StoreProjectRequest extends FormRequest
+{
+    public function authorize()
+    {
+        abort_if(Gate::denies('project_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+            ],
+        ];
+    }
+}
