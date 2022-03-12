2601c61f31dd062983c37885e0b95033d98a723d
Fay Johnston <swewrozka@gmail.com>
2022-03-12 09:19:13 -0500

    add document and user routes

--- project-anemone/routes/api.php
+++ project-anemone/routes/api.php
@@ -23,6 +23,60 @@ Route::group([
     Route::apiResource('projects', 'ProjectsApiController');
 });
 
+// user routes
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('auth:sanctum')->post('/user', function(Request $request){
+  return $request->create_user();
+});
+
+Route::middleware('auth:sanctum')->post('/user/[]', function(Request $request){
+  return $request->create_user_array();
+});
+
+Route::middleware('auth:sanctum')->get('/user/logout', function(Request $request){
+  return $request->logout();
+});
+
+Route::middleware('auth:sanctum')->get('/user/{username}', function(Request $request){
+  return $request->get_username();
+});
+
+Route::middleware('auth:sanctum')->put('/user/{username}', function(Request $request){
+  return $request->get_username_by_username();
+});
+
+Route::middleware('auth:sanctum')->delete('/user/{username}', function(Request $request){
+  return $request->delete_user();
+});
+
+// document routes
+Route::middleware('auth:sanctum')->post('/upload', function(Request $request){
+  return $request->upload();
+});
+
+Route::middleware('auth:sanctum')->put('/document', function(Request $request){
+  return $request->update_document();
+});
+
+Route::middleware('auth:sanctum')->get('/document/status', function(Request $request){
+  return $request->find_document_by_status();
+});
+
+Route::middleware('auth:sanctum')->get('/document/{documentId}', function(Request $request){
+  return $request->get_document_by_id();
+});
+
+Route::middleware('auth:sanctum')->delete('/document/{documentId}', function(Request $request){
+  return $request->delete_document_by_id();
+});
+
+Route::middleware('auth:sanctum')->post('/document/[]', function(Request $request){
+  return $request->create_document_list();
+});
+
+// search route
+Route::middleware('auth:sanctum')->get('/document/search/{path}', function(Request $request){
+  return $request->search_documents_by_query();
 });
