d294831d6fed21622c8849eede06864c2092b14e
Necosama <tug65044@temple.com>
2022-03-13 15:03:57 -0400

    slight change

--- project-anemone/resources/views/index.php
+++ project-anemone/resources/views/index.php
@@ -18,18 +18,13 @@
                 </div>
                 <div class="nav">
                     <div class="extension">
-                        <a href="#">
-                            //<img src="../imgs/icon/list.svg" alt="">
+                        <a href="login" title="log in">
+                            <img src="../imgs/icon/user.png" alt=""><p>user name</p>
                         </a>
-                        <a href="#">
-                            <img src="../imgs/icon/symbol.png" alt="">
-                        </a>
-                        <a href="#">
+                        
+                        <!-- <a href="#" title="add a note">
                             <img src="../imgs/icon/book-alt.svg" alt="">
-                        </a>
-                        <a href="#">
-                            <img src="../imgs/icon/plus.svg" alt="">
-                        </a>
+                        </a> -->
                     </div>
                     <div class="more">
                         <div class="title">
@@ -118,7 +113,15 @@
                 </div>
             </div>
         </div>
-        <div id="display" class="float-right"></div>
+        <div id="display" class="float-right">
+            <form class="note-form" action="">
+                <input class="note-info" type="text" name="note-lable">
+                <input class="addbtn" type="submit" value="add">
+                <div><p class="note1">node 1:Lorem ipsum dolor sit amet.</p><input type="submit" value="delete"></div>
+                <div><p class="note2">node 1:Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.s</p><input type="submit" value="delete"></div>
+                <div><p class="note1">node 1:Lorem ipsum dolor sit amet.</p><input type="submit" value="delete"></div>
+            </form>
+        </div>
     </div>
 </body>
 
