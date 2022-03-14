92f3c6920e4eb8ea09dbe8841dce8b986bc7c69b
Fay Johnston <swewrozka@gmail.com>
2022-03-13 23:11:29 -0400

    add document db w/ successful migrate script

---
+++ project-anemone/database/migrations/2022_03_14_022342_add_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->json('file_metadata');
+            $table->text('title');
+            $table->text('file_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
