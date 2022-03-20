0e345869e1b9c5a9043ba9ddd0abea149598230c
Necosama <70273062+Necosama@users.noreply.github.com>
2022-03-20 17:13:16 -0400

    Add files via upload

---
+++ project-anemone/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller{
+    public function index(){
+        return view('login');
+    }
+
+    public function sign_in(Request $request){
+        $this->validate($request, [
+            //'username' => 'required|max:255',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if(auth()->attempt($request->only('email','password'))){
+            return redirect()->route('dashboard');
+        }
+    }
+}
