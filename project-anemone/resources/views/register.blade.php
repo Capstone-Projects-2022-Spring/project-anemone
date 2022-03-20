8858ecff139b3e6677a4b00505b71541ea41a654
faith-ima <77637834+faith-ima@users.noreply.github.com>
2022-03-20 12:28:08 -0400

    removed coupon and contact us blocks

--- project-anemone/resources/views/register.blade.php
+++ project-anemone/resources/views/register.blade.php
@@ -128,71 +128,8 @@
                 </div>
         </section>
 
-        <section class="section-homepage-subscribe">
-            <div class="container">
-                <div class="big-circle">
+        <!-- add issue collector here -->
 
-                    get the
-                    <div class="big"><span>$</span>10</div>
-                    coupon
-
-                </div>
-                <div class="offer-text">
-                    Sign in for our newsletter and recieve a ten dollars coupon
-                </div>
-                <div class="email-holder">
-
-                    <div class="email-field">
-
-                        <form>
-                            <input class="required email placeholder" name="email" data-placeholder="Enter here your email address...">
-                            <button class="newsletter-submit-btn" type="submit" value=""><i class="icon-plus"></i></button>
-
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="section-footer">
-            <div class="container">
-                <div class="row-fluid">
-                    <div class="span3">
-
-                    </div>
-                    <div class="span3">
-
-                    </div>
-                    <div class="span3">
-
-                    </div>
-                    <div class="span3">
-                        <div class="footer-links-holder">
-                            <h2>get in touch</h2>
-                            <p>
-                                Cosmetico Shop<br> Good Town 122, Beaty Centre<br> (011) 212 222 22
-                            </p>
-                            <ul class="inline social-icons">
-                                <li>
-                                    <a href="#" class="icon-facebook"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="icon-google-plus"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="icon-rss"></a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="icon-linkedin"></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
         <section class="section-copyright">
             <div class="container">
                 <div class="copyright pull-left">
@@ -209,29 +146,6 @@
         </section>
 
         </div>
-
-
-        <!-- <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
-        <script src="js/jquery-migrate-1.1.1.min.js"></script>
-        <script src="css/bootstrap/js/bootstrap.min.js"></script>
-
-        <script type="text/javascript" src="js/css_browser_selector.js"></script>
-        <script src="js/jquery.icheck.min.js"></script>
-        <script type="text/javascript" src="js/twitter-bootstrap-hover-dropdown.min.js"></script>
-        <script type="text/javascript" src="js/jquery.easing-1.3.js"></script>
-        <script type="text/javascript" src="js/jquery.validate.js"></script>
-
-
-        <script type="text/javascript" src="js/bootstrap-slider.js"></script>
-
-
-        <script type="text/javascript" src="js/script.js"></script> -->
-
-
-
-
-
-
     </body>
 </section>
 
