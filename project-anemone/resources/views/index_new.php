90d325e5f8a2325372f2bbff113ba0c478d258c4
Necosama <tug65044@temple.com>
2022-03-23 22:23:25 -0400

    New index page updated, visit localhost/index_new.php for reviewing plz.

---
+++ project-anemone/resources/views/index_new.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anemone</title>
+    <link rel="stylesheet" href="../css/index_new.css">
+</head>
+
+<body>
+    <div id="container">
+        <div class="top">
+            <div class="wrapper clearfix">
+                <div class="title float-left"><h1>Anemone</h1></div>
+                <div class="user float-right">
+                    <a href="login"><img src="../imgs/icon/user.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="nav clearfix">
+                <div class="search float-left">
+                    <form action="">
+                        <input type="text" placeholder="Search">
+                    </form>
+                </div>
+                <div class="funs float-left clearfix">
+                    <div class="float-left"><a href=""><img src="../imgs/icon/book-alt.svg" alt=""></a></div>
+                    <div class="float-left"><a href=""><img src="../imgs/icon/bookmark.svg" alt=""></a></div>
+                    <div class="float-left"><a href=""><img src="../imgs/icon/forward.svg" alt=""></a></div>
+                    <div class="float-left"><a href=""><img src="../imgs/icon/beforeLike.svg" alt=""></a></div>
+                </div>
+            </div>
+
+            <div class="display clearfix">
+                <div class="doc float-left">
+                    <div class="title"><h2>DOCUMENTATION</h2></div>
+                    <div class="each-doc"><a href=""><img src="../imgs/icon/angle-circle-right.svg" alt=""> <span>Docker Collection</span></a></div>
+                    <div class="each-doc"><a href=""><img src="../imgs/icon/angle-circle-right.svg" alt=""> <span>Recently Visited</span></a></div>
+                    <div class="each-doc"><a href=""><img src="../imgs/icon/angle-circle-right.svg" alt=""> <span>EBP Group</span></a></div>
+                    <div class="each-doc"><a href=""><img src="../imgs/icon/angle-circle-right.svg" alt=""> <span>Recommended</span></a></div>
+                    <div class="each-doc"><a href=""><img src="../imgs/icon/angle-circle-right.svg" alt=""> <span>Bookmarks</span></a></div>
+                </div>
+                <div class="result float-right">
+                    <div class="search-result">
+                        <div class="title"><h2>PAGINATED RESULTS</h2></div>
+                        <div class="each-doc"><a href=""><span>Public Search Result2</span></a></div>
+                        <div class="each-doc"><a href=""><span>Personal Search Result 1</span></a></div>
+                        <div class="each-doc"><a href=""><span>Public Search Result 3</span></a></div>
+                        <div class="each-doc"><a href=""><span>Entitled Search Result 2</span></a></div>
+                        <div class="each-doc"><a href=""><span>Public Search Result 1</span></a></div>
+                    </div>
+                    <div class="report-result">
+                        <div class="title"><h2>Report Bug Pop-up</h2></div>
+                    </div>
+                </div>
+                <div class="view">
+                    <div  class="data-view"></div>
+                    <div class="annotation">
+                        <div class="title"><h2>Annotation to Upload</h2></div>
+                        <a href=""><img src="../imgs/icon/add.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
