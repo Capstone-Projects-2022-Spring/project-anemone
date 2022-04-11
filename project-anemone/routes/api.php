2e0d91d77c14458013a00f3a22f55d38f33083d0
Thomas Rowe <tuh07745@temple.edu>
2022-04-11 02:44:53 -0400

    Added/updated routes to api.php
    Not entirely sure how I would go about testing these changes

--- project-anemone/routes/api.php
+++ project-anemone/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +28,38 @@ Route::group([
   'middleware' => ['auth:api']
 ], function () {
     Route::apiResource('anemone', 'ApiController');
+});
+
+Route::post('/register', function(Request $request){
+  return RegisterController::register_user($request);
+});
+
+Route::get('/login', function(Request $request){
+  return LoginController::sign_in($request);
+});
+
+Route::get('/logout', function(){
+  return LogoutController::log_out();
+});
+
+Route::get('/email/verify', function() {
+    return view('auth.emailverify');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/index');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+Route::get('/documents', function(Request $request){
+  return DocumentController::upload($request);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
