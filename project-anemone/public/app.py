b2e13ba0903d551542342af758fb22217a4b2d1f
Thomas Rowe <tuh07745@temple.edu>
2022-04-21 16:02:18 -0400

    Update app.py

--- project-anemone/public/app.py
+++ project-anemone/public/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import Scraper
 
 app = Flask(__name__)
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def scrape():
     url = request.form['url']
     user_id = request.form['user_id']
-    Scraper.main(url, user_id)
-    return "201"
+    data = Scraper.main(url, user_id)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=False, host= '0.0.0.0')
